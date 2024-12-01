9cdff9324cc01b40716c1e10597c5cf488afa7cb
Dylan Jurski <jurskidylan@yahoo.com>
2024-12-01 15:01:34 -0500

    Day 1

---
+++ src/day1.rs
@@ -0,0 +1,41 @@
+#[aoc(day1, part1)]
+pub fn day1(input: &str) -> u32 {
+    
+    // let mut left = Vec::with_capacity(1000);
+    // let mut right = Vec::with_capacity(1000);
+    
+    // input
+    // .lines()
+    // .map(|line| unsafe {
+    //     (
+    //         u32::from_str_radix(&line[0..5], 10).unwrap_unchecked(),
+    //         u32::from_str_radix(&line[8..13], 10).unwrap_unchecked(),
+    //     )
+    // })
+    // .for_each(|(lhs, rhs)| {
+    //     left.push(lhs);
+    //     right.push(rhs);
+    // });
+    
+    let mut left: [u32; 1000] = [0;1000];
+    let mut right: [u32; 1000] = [0;1000];
+
+    let mut count = 0;
+
+    let _: Vec<_> = input
+        .lines()
+        .map(|line| {
+            unsafe {
+                left[count] = u32::from_str_radix(&line[0..5], 10).unwrap_unchecked();
+                right[count] = u32::from_str_radix(&line[8..13], 10).unwrap_unchecked();
+            }
+            count +=1;
+        }).collect();
+
+    left.sort_unstable();
+    right.sort_unstable();
+
+    left.iter()
+        .zip(right)
+        .fold(0, |acc, (lhs, rhs)| acc + lhs.abs_diff(rhs))
+}
