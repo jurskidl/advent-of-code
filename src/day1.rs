7ad1671ad536d4a0ac7a9624d6a209385f77ba0e
Dylan Jurski <jurskidylan@yahoo.com>
2025-01-08 13:50:28 -0500

    Updated day 1 to use simd
    Use of simd to try and learn. Adaptation of alion02 code to try and learn simd

--- src/day1.rs
+++ src/day1.rs
@@ -1,31 +1,77 @@
-fn parse_line(line: &[u8]) -> (u32, u32) {
-    (
-        (0_usize..5_usize)
-            .into_iter()
-            .map(|x| unsafe {
-                ((*line.get_unchecked(x) as u32 - 48u32) * 10u32.pow(((5 - 1) - x) as u32)) as u32
-            })
-            .sum(),
-        (8_usize..13_usize)
-            .into_iter()
-            .map(|x| unsafe {
-                ((*line.get_unchecked(x) as u32 - 48u32) * 10u32.pow(((13 - 1) - x) as u32)) as u32
-            })
-            .sum(),
+use std::arch::x86_64::{_mm256_madd_epi16, _mm256_maddubs_epi16};
+use std::simd::prelude::*;
+
+unsafe fn parse_numbers(two_lines:Simd<u8, 32>) -> (u32, u32, u32, u32) {
+    let adj_chunk = simd_swizzle!(two_lines, [
+        00, 01, 02, 03, 04, 05, 06, 07, //
+        08, 09, 10, 11, 12, 13, 05, 05, //
+        14, 15, 16, 17, 18, 19, 20, 21, //
+        22, 23, 24, 25, 26, 27, 19, 19, //
+    ])- u8x32::from_array([
+        b'0', b'0', b'0', b'0', b'0', b' ', b' ', b' ', //
+        b'0', b'0', b'0', b'0', b'0', b'\n', b' ', b' ', //
+        b'0', b'0', b'0', b'0', b'0', b' ', b' ', b' ', //
+        b'0', b'0', b'0', b'0', b'0', b'\n', b' ', b' ', //
+    ]);
+    let step1 = unsafe{ _mm256_maddubs_epi16(
+        adj_chunk.into(),
+        u8x32::from_array([
+            10, 1, 10, 1, 1, 0, 0, 0, //
+            10, 1, 10, 1, 1, 0, 0, 0, //
+            10, 1, 10, 1, 1, 0, 0, 0, //
+            10, 1, 10, 1, 1, 0, 0, 0, //
+        ])
+        .into(),
+    ) };
+    let step2: u16x16 = unsafe {_mm256_madd_epi16(
+        step1,
+        u16x16::from_array([
+            100, 1, 1, 0, //
+            100, 1, 1, 0, //
+            100, 1, 1, 0, //
+            100, 1, 1, 0, //
+            ]).into(),
     )
+    .into()};
+    let step3 =
+        simd_swizzle!(step2, [
+        00, 02, 04, 06, //
+        00, 02, 04, 06, //
+        08, 10, 12, 14, //
+        08, 10, 12, 14, //
+        ]).into();
+
+    let fin: u32x8 =
+        _mm256_madd_epi16(
+            step3,
+            u16x16::from_array([
+                10, 1, 10, 1, 0, 0, 0, 0, 
+                10, 1, 10, 1, 0, 0, 0, 0
+                ]).into(),
+        )
+        .into();
+
+    (fin[0],fin[4],fin[1],fin[5])
 }
 
-fn scan_file(input: &[u8]) -> ([u32; 1000], [u32; 1000]) {
+fn scan_file(input: &[u8]) -> ([u32;1000], [u32;1000]) {
     let mut first: [u32; 1000] = [0; 1000];
     let mut second: [u32; 1000] = [0; 1000];
 
-    for line in 0..1000 {
-        unsafe {
+    let mut i = 0;
+    let mut index = 1;
+    while i < 13999 - 14 {
+        let two_lines = unsafe{ (input.get_unchecked(i) as *const _ as *const u8x32).read_unaligned() };
+        unsafe { 
             (
-                *first.get_unchecked_mut(line),
-                *second.get_unchecked_mut(line),
-            ) = parse_line(&input[(line * 14)..(line * 14) + 13]);
+                *first.get_unchecked_mut(index-1),
+                *first.get_unchecked_mut(index),
+                *second.get_unchecked_mut(index-1),
+                *second.get_unchecked_mut(index),
+            ) = parse_numbers(two_lines)
         }
+        i += 28;
+        index += 2;
     }
 
     first.sort_unstable();
@@ -45,36 +91,77 @@ pub fn part1(input: &str) -> u32 {
         .fold(0, |acc, (one, two)| acc + one.abs_diff(two))
 }
 
-fn parse_line2(line: &[u8]) -> (usize, usize) {
-    (
-        (0_usize..5_usize)
-            .into_iter()
-            .map(|x| unsafe {
-                ((*line.get_unchecked(x) as usize - 48usize) * 10usize.pow(((5 - 1) - x) as u32))
-                    as usize
-            })
-            .sum(),
-        (8_usize..13_usize)
-            .into_iter()
-            .map(|x| unsafe {
-                ((*line.get_unchecked(x) as usize - 48usize) * 10usize.pow(((13 - 1) - x) as u32))
-                    as usize
-            })
-            .sum(),
+unsafe fn parse_numbers2(two_lines:Simd<u8, 32>) -> (usize, usize, usize, usize) {
+    let adj_chunk = simd_swizzle!(two_lines, [
+        00, 01, 02, 03, 04, 05, 06, 07, //
+        08, 09, 10, 11, 12, 13, 05, 05, //
+        14, 15, 16, 17, 18, 19, 20, 21, //
+        22, 23, 24, 25, 26, 27, 19, 19, //
+    ])- u8x32::from_array([
+        b'0', b'0', b'0', b'0', b'0', b' ', b' ', b' ', //
+        b'0', b'0', b'0', b'0', b'0', b'\n', b' ', b' ', //
+        b'0', b'0', b'0', b'0', b'0', b' ', b' ', b' ', //
+        b'0', b'0', b'0', b'0', b'0', b'\n', b' ', b' ', //
+    ]);
+    let step1 = unsafe{ _mm256_maddubs_epi16(
+        adj_chunk.into(),
+        u8x32::from_array([
+            10, 1, 10, 1, 1, 0, 0, 0, //
+            10, 1, 10, 1, 1, 0, 0, 0, //
+            10, 1, 10, 1, 1, 0, 0, 0, //
+            10, 1, 10, 1, 1, 0, 0, 0, //
+        ])
+        .into(),
+    ) };
+    let step2: u16x16 = unsafe {_mm256_madd_epi16(
+        step1,
+        u16x16::from_array([
+            100, 1, 1, 0, //
+            100, 1, 1, 0, //
+            100, 1, 1, 0, //
+            100, 1, 1, 0, //
+            ]).into(),
     )
+    .into()};
+    let step3 =
+        simd_swizzle!(step2, [
+        00, 02, 04, 06, //
+        00, 02, 04, 06, //
+        08, 10, 12, 14, //
+        08, 10, 12, 14, //
+        ]).into();
+
+    let fin: u32x8 =
+        _mm256_madd_epi16(
+            step3,
+            u16x16::from_array([
+                10, 1, 10, 1, 0, 0, 0, 0, 
+                10, 1, 10, 1, 0, 0, 0, 0
+                ]).into(),
+        )
+        .into();
+
+    (fin[0] as usize,fin[4] as usize,fin[1] as usize,fin[5] as usize)
 }
 
-fn scan_file2(input: &[u8]) -> ([usize; 1000], [usize; 1000]) {
+fn scan_file2(input: &[u8]) -> ([usize;1000], [usize;1000]) {
     let mut first: [usize; 1000] = [0; 1000];
     let mut second: [usize; 1000] = [0; 1000];
 
-    for line in 0..1000 {
-        unsafe {
+    let mut i = 0;
+    let mut index = 1;
+    while i < 13999 - 14 {
+        let two_lines = unsafe{ (input.get_unchecked(i) as *const _ as *const u8x32).read_unaligned() };
+        unsafe { 
             (
-                *first.get_unchecked_mut(line),
-                *second.get_unchecked_mut(line),
-            ) = parse_line2(&input[(line * 14)..(line * 14) + 13]);
+                *first.get_unchecked_mut(index-1),
+                *first.get_unchecked_mut(index),
+                *second.get_unchecked_mut(index-1),
+                *second.get_unchecked_mut(index),
+            ) = parse_numbers2(two_lines)
         }
+        i += 28;
+        index += 2;
     }
 
     first.sort_unstable();
